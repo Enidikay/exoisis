4a29701ee57f52409191e9c29b932a1828c69e70
Enidikay <abdallah.yakidine@gmail.com>
2024-10-10 16:20:27 +0200

    fini

--- exos/exercice2.ts
+++ exos/exercice2.ts
@@ -37,3 +37,91 @@ function mult_table(n:number):number{
 }
 
 //console.log(mult_table(5));
+
+/*d. Écrire une fonction factorial qui prend un entier n et renvoie sa factorielle tel que
+n! = n * n-1* n-2 * ... * 2*/
+
+function factorial(n:number):number{
+    let res:number = 1
+    for(let i = 0; i<n; i++){
+        res = res *(i+1)
+    }
+    return res;
+}
+
+//console.log(factorial(10));
+
+function pow(x:number, n:number):number{
+    let res:number = 1;
+
+    for(let i = 0; i<n; i++){
+        res = res * x
+    }
+    return res;
+}
+//console.log(pow(2,2));
+
+function sum_even(n:number):number{
+    let res:number = 0;
+
+    for(let i = 0; i<n; i++){
+        if ((i+1)%2 == 0){
+            res = res + (i+1)
+        }
+    }
+    return res;
+}
+
+//console.log(sum_even(10))
+
+function sum_arr (a:number[]):number{
+    let res:number = 0
+
+    for(let i = 0; i<a.length; i++){
+        res = a[i] + res
+    }
+    return res
+}
+
+//console.log(sum_arr([10,5,2,6]))
+
+function isPrime(n:number):boolean{
+    for(let i = 2; i<n; i++){
+        if (n%i == 0){
+            return false
+        }
+    }
+    return true
+}
+
+//console.log(isPrime(12))
+
+function pgcd(x:number, y:number):number{
+    let res:number = 0
+    let min = Math.min(x,y)
+    for(let i = 1;i<min; i++){
+        if (x%i == 0 && y%i == 0){
+            res = i
+        }
+    }
+    return res;
+}
+
+//console.log(PGCD(32,24));
+
+function is_perfect(n:number):boolean{
+    let res:number = 0
+    for(let i = 1; i<n; i++){
+        if (n%i == 0){
+            res = res+i
+        }
+    }
+    if(res == n){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(is_perfect(28));
